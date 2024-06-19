912e1be899ff745b87e3125abc7aabb31cbca789
vandana104 <vandanaoel@gmail.com>
2024-06-19 18:34:24 +0530

    Initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// MoEngage Assignment
+
+// Bewery Review System
+// 1. Build a web application to search Brewery by City using Brewery APIs
+// (https://www.openbrewerydb.org/documentation) and add reviews. This web app
+// should have sign-up for new users and login for existing users.
+// 2. When a user logs in, the home page should have a Brewery search option. This
+// search option should be able to search by_city, by_name, and by_type (Read
+// API docs to see what by_city, by_name, and by_type means).
+// 3. The search result should display at least the following details per Brewery:
+// a. Brewery name
+// b. Brewery Address
+// c. Phone number
+// d. Web site URL
+// e. Current rating (rating based on point no.4)
+// f. State, City
+// 4. When the user clicks on a Brewery, the new page should open with all possible
+// Brewery information. The user should have the ability to add reviews for a
+// Brewery, and this should be saved in a hosted database that you will host to
+// complete this exercise. Whenever someone opens a brewery page, the reviews
+// should also be shown on the brewery info page. You should be storing the new
+// reviews given by the user in your database and show them in the info page when
+// it opens.
+// 5. The review should contain a rating from 1-5 and a description.
+
+// The application should have the following pages
+// 1. Login/Signup Page
+// 2. Search Page
+// 3. Brewery page, along with existing reviews pulled from the hosted database. And
+// also a section to add a review.
+// What to submit
+// 1. Please send us the video of the working demo of the application. The video should be
+// accessible to all the people who have the link.
+// 2. Please send us the code. Please share the public GitHub code link.
+// 3. Please show your database contents in the video.
+
+// We'd prefer this hosted on any free hosting server instead of localhost. You can choose
+// any stack you are comfortable with to complete the assignment.
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
     </div>
   );
 }
 
 export default App;
+
+
+
